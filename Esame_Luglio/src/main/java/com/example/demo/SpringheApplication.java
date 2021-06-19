a36a32d11ed0e67e555e8a96e79dd168539445fe
Matteo Giacomobono <Matteo Giacomobono@LAPTOP-GVR5TMIV.station>
2021-06-19 10:46:39 +0200

    Project creation

---
+++ Esame_Luglio/src/main/java/com/example/demo/SpringheApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringheApplication.class, args);
+	}
+
+}
